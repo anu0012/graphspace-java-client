c95d4253a0db763164ca3cbf5f56fc42b27fa938
anu0012 <anurag30671371@gmail.com>
2017-04-22 10:58:35 +0530

    implemented DELETE method

--- src/com/graphspace/javalibrary/ClientApi.java
+++ src/com/graphspace/javalibrary/ClientApi.java
@@ -9,11 +9,14 @@ import java.nio.charset.Charset;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
  
 public class ClientApi
 {
@@ -38,6 +41,7 @@ public class ClientApi
         data.put("is_public", isPublic);
         data.put("owner_email", username);
         data.put("graph_json", g.computeGraph());
+        data.put("id", 12345);
         data.put("style_json",g.computeGraphStyle());
         
         String jsonData = data.toString();
@@ -103,7 +107,58 @@ public class ClientApi
         catch(Exception e){
         	e.printStackTrace();
         }
+        
+        
         return result.toString();
+    }
+    
+    // DELETE Method
+    public String deleteGraph(String graphName){
+    	//TODO use getGraph to get JSON of the graph and retrieve id from parsed JSON
+    	String id="";
+    	JSONObject jsonObject = (JSONObject) JSONValue.parse(getGraph(graphName,username));
+    	long total = (Long) jsonObject.get("total");
+    	if(total > 0)
+    	{
+    		JSONObject graph = (JSONObject)((JSONArray)jsonObject.get("graphs")).get(0);
+    		if(graph.get("name").equals(graphName))
+    		{
+    			id = graph.get("id")+"";
+    		}
+    		else{
+    			graph = (JSONObject)((JSONArray)jsonObject.get("graphs")).get((int)total-1);
+    			id = graph.get("id")+"";
+    		}
+    	}
+    	
+    	System.out.println(id);
+    	
+    	HttpDelete httpDelete = new HttpDelete(API_HOST+id);
+    	String auth=new StringBuffer(username).append(":").append(password).toString();
+        byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+        String authHeader = "Basic " + new String(encodedAuth);
+        httpDelete.setHeader("AUTHORIZATION", authHeader);
+        httpDelete.setHeader("Content-Type", "application/json");
+        httpDelete.setHeader("Accept", "application/json");
+        
+        HttpResponse response=null;
+        String line = "";
+        StringBuffer result = new StringBuffer();
+        
+        HttpClient client = HttpClientBuilder.create().build();
+        try{
+        response = client.execute(httpDelete);
+        
+        System.out.println("Response Code : " +response.getStatusLine().getStatusCode());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+        while ((line = reader.readLine()) != null){ result.append(line); }
+        System.out.println(result.toString());
+        }
+        catch(Exception e){
+        	e.printStackTrace();
+        }
+        return result.toString();
+        
     }
     
 }
